b41fa5f303f609fbfea09e94a3214539cda956bc
kuaka <stuart.mackintosh@gmail.com>
2020-11-22 14:39:07 +1300

    initial commit

---
+++ fsdb_igc.py
@@ -0,0 +1,79 @@
+# !/usr/bin/env python
+#
+import lxml.etree as ET
+import os, argparse
+import shutil
+
+
+def main(args):
+
+    if not os.path.isfile(args.file):
+        print("fsdb file does not exist")
+        exit()
+
+    path = os.path.dirname(args.file)
+
+    try:
+        tree = ET.parse(args.file)
+    except TypeError:
+        tree = ET.parse(args.file.as_posix())
+    except ET.Error:
+        print("FSDB Read Error.")
+        return None
+    finally:
+        root = tree.getroot()
+
+    participants = root.xpath('FsCompetition/FsParticipants')
+    tasks = root.xpath('FsCompetition/FsTasks')
+    pilots = {}
+    for participant in participants[0]:
+        pilots[participant.attrib.get('id')] = participant.attrib.get('name')
+    print(pilots)
+
+    destination = os.path.join(path, "output")
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    for task in tasks[0]:
+        source_dir = task.get('tracklog_folder')
+        print(task.get('name'))
+        task_destination = os.path.join(destination, source_dir)
+        if not os.path.exists(task_destination):
+            os.makedirs(task_destination)
+        source = None
+        if dir:
+            source = os.path.join(path, source_dir)
+        if not os.path.isdir(source):
+            print(f"{source} does not exist")
+            exit()
+        task_participants = task.xpath('FsParticipants')
+
+        for participant in task_participants[0]:
+            pil_id = participant.get('id')
+            name = pilots[str(pil_id)].replace(" ", "_")
+            if participant.xpath('FsFlightData'):
+                tracklog = participant.xpath('FsFlightData')[0].get('tracklog_filename')
+                print(f'{pil_id=} {name=} {tracklog=}')
+                if tracklog:
+                    if len(tracklog) > 5:
+                        _, extension = os.path.splitext(tracklog)
+                        out = os.path.join(task_destination, name + extension)
+                        shutil.copy2(os.path.join(source, tracklog), out)
+
+        shutil.make_archive(os.path.join(destination, source_dir), 'zip', task_destination)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Reads a fsdb file and copies the igc files that have been used in the file to a new folder"
+                    " renaming them firstname_lastname.igc where the name is the name of the pilot."
+                    "The source igc files should be located in sub dirs to the location of the fsdb. The dirs should "
+                    "be named as they are in the fsdb",)
+
+    parser.add_argument(
+        # "-f"
+        "file",
+        help="The fsdb file to process.",
+    )
+    args = parser.parse_args()
+    main(args)
---
+++ readme.md
@@ -0,0 +1,15 @@
+###### FSDB IGC
+
+A simple script to take igc files nominated in a FSDB file and rename them with the pilots name. Creates folders for each task with cleanly named trackfiles.
+It also creates a zip file for each task. 
+The zip file is suitable for bulk uploading to airScore.
+
+
+Requirements:
+- Python 3
+- lxml (install with 'pip install lxml')
+
+Usage: python fsdb_igc.py /path/to/fsdbfile.fsdb
+
+In the dir where the fsdb file is stored, fsdb_igc will expect to find sub dirs with tracklogs. The sub dir names and tracklog names need to be as they are specified in the fsdb file.
+fsdb_igc will create an output dir with zip files in the root and sub dirs (named as above) with the clean tracklog files inside.
